66481404dcf094d8e959123063fd248127aacec7
datawithyandy <yandy.plasencia@datahavenanalytics.com>
2020-10-04 12:16:43 -0400

    3 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,33 @@
-import { Component, VERSION } from '@angular/core';
+import { Component, OnInit,  VERSION } from '@angular/core';
 
 @Component({
   selector: 'my-app',
   templateUrl: './app.component.html',
   styleUrls: [ './app.component.css' ]
 })
-export class AppComponent  {
+export class AppComponent implements OnInit {
   name = 'Angular ' + VERSION.major;
+
+  ngOnInit(){
+
+
+
+
+  }
+
 }
+
+
+/*
+
+$(function(){
+	plotConceptMap();
+});
+function plotConceptMap()
+{
+	d3.json("metadata.json", function(dataJson) {
+		var plot = new ConceptMap("graph", "graph-info", dataJson);
+	});
+}
+
+*/
